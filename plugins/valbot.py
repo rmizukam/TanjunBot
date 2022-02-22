59718c1b79076077c9c1242bf01eb51f1a9a216c
Ryan Mizukami <rmizukami741@gmail.com>
2022-02-21 22:35:14 -0800

    valbot properly displays however only envoker can interact with embed

--- plugins/valbot.py
+++ plugins/valbot.py
@@ -1,15 +1,22 @@
+from __future__ import annotations
 import asyncio
-from attr import field
-import hikari
-import tanjun
+import logging
 
+from typing import Any, Callable
+from functools import partial
+from unicodedata import name
+
+import hikari
+from hikari.traits import EntityFactoryAware
 from hikari import InteractionCreateEvent
 from hikari.interactions.base_interactions import ResponseType
+from hikari.embeds import Embed
 from hikari.messages import ButtonStyle
-
+import tanjun
 from tanjun.abc import SlashContext
-import random
-from functions import stratChoice, unload_csv
+
+from hikari.events.message_events import GuildMessageCreateEvent
+from functions import embedStratChoice, unload_csv
 
 acount = set()
 dcount = set()
@@ -30,8 +37,9 @@ dStratArray = generalStrategiesArray + defenseStrategiesArray
 
 
 EMBED_MENU = {
-    "📋": {"title": "Attack", "style": ButtonStyle.SECONDARY},
-    '🖊️': {'title': 'Defense', 'style': ButtonStyle.SECONDARY},
+    "⚔": {"title": "Attack", "style": ButtonStyle.SECONDARY},
+    '🛡': {'title': 'Defense', 'style': ButtonStyle.SECONDARY},
+    "🗺": {'title': 'Choose Map', 'style': ButtonStyle.SECONDARY},
     "❌": {"title": "Cancel", "style": ButtonStyle.DANGER}
 }
 
@@ -47,8 +55,8 @@ async def valbot(
     bot: hikari.GatewayBot = tanjun.injected(type=hikari.GatewayBot),
     client: tanjun.Client = tanjun.injected(type=tanjun.Client)
 ) -> None:
-    building_embed = hikari.Embed(title="Strat Roulette",
-                                  description='Click/Tap your choice below.')
+    building_embed = hikari.Embed(title="Choose Map")
+    building_embed.add_field('Strat Roulette', value='strategy')
     
 
     await embed_builder_loop(ctx, building_embed, bot=bot, client=client)
@@ -107,43 +115,85 @@ def build_menu(ctx: SlashContext):
     return menu
 
 
-async def attack(ctx: SlashContext, bot: hikari.GatewayBot, client: tanjun.Client):
+async def choose_map(ctx: SlashContext, bot: hikari.GatewayBot, client: tanjun.Client):
     embed_dict, *_ = bot.entity_factory.serialize_embed(client.metadata['embed'])
-    global dcount, dNamesArray, dStratArray
-    roll, dcount = stratChoice(dNamesArray, dStratArray, dcount)
-    await ctx.edit_initial_response(content=roll, components=[])
-    try:
-        async with bot.stream(hikari.GuildMessageCreateEvent, timeout=60).filter(('author', ctx.author)) as stream:
-            async for event in stream:
-                embed_dict['title'] = event.content[:200]
-                client.metadata['embed'] = bot.entity_factory.deserialize_embed(embed_dict)
-                await ctx.edit_initial_response(content="Title updated!", embed=client.metadata['embed'], components=[])
-                await event.message.delete()
-                return
-    except asyncio.TimeoutError:
-        await ctx.edit_initial_response("Waited for 60 seconds... Timeout.", embed=None, components=[])
+    await ctx.edit_initial_response(content="Set Map:", components=[])
+    event = await collect_response(ctx)
+    embed_dict['title'] = event.content[:200]
+    client.metadata['embed'] = bot.entity_factory.deserialize_embed(embed_dict)
+    # print(embed_dict['title'])
+    print(embed_dict)
+    await event.message.delete()
 
 
 async def defense(ctx: SlashContext, bot: hikari.GatewayBot, client: tanjun.Client):
     embed_dict, *_ = bot.entity_factory.serialize_embed(client.metadata['embed'])
-    await ctx.edit_initial_response(content='Defense', components=[])
-    async with bot.stream(hikari.GuildMessageCreateEvent, timeout=60).filter(('author', ctx.author)) as stream:
-        async for event in stream:
-            global acount, aNamesArray, aStratArray
-            roll, acount = stratChoice(aNamesArray, aStratArray, acount)
-            client.metadata['embed'] = bot.entity_factory.deserialize_embed(embed_dict)
-            await ctx.edit_initial_response(content=roll, embed=client.metadata['embed'], components=[])
-            await event.message.delete()
-            return
+    await ctx.edit_initial_response(content="You Chose Defense:", components=[])
+    global dcount, dNamesArray, dStratArray
+    nameRoll, stratRoll, dcount = embedStratChoice(dNamesArray, dStratArray, dcount)
+    client.metadata['embed'].edit_field(0, nameRoll, stratRoll, inline=False)
+    await ctx.edit_initial_response(content="Good Luck!", embed=client.metadata['embed'], components=[])
 
 
-# async def attack(ctx: SlashContext, bot: hikari.GatewayBot, client: tanjun.Client):
-#     embed_dict, *_ = bot.entity_factory.serialize_embed(client.metadata['embed'])
-#     await ctx.edit_initial_response(content='Attack', components=[])
-#     async with bot.stream(hikari.GuildMessageCreateEvent, timeout=60).filter(('author', ctx.author)) as stream:
-#         global dcount, dNamesArray, dStratArray
-#         roll, dcount = stratChoice(dNamesArray, dStratArray, dcount)
-#         await ctx.respond(roll)
+async def attack(ctx: SlashContext, bot: hikari.GatewayBot, client: tanjun.Client):
+    embed_dict, *_ = bot.entity_factory.serialize_embed(client.metadata['embed'])
+    await ctx.edit_initial_response(content="You Chose Attack:", components=[])
+    global acount, aNamesArray, aStratArray
+    nameRoll, stratRoll, acount = embedStratChoice(aNamesArray, aStratArray, acount)
+    client.metadata['embed'].edit_field(0, nameRoll, stratRoll, inline=False)
+    await ctx.edit_initial_response(content="Good Luck!", embed=client.metadata['embed'], components=[])
+
+
+async def collect_response(ctx: SlashContext, validator: list[str] | Callable | None = None, timeout: int = 60, timeout_msg: str = "Waited for 60 seconds... Timeout.") -> GuildMessageCreateEvent | None:
+    def is_author(event: GuildMessageCreateEvent):
+        if ctx.author == event.message.author:
+            return True
+        return False
+    while True:
+        try:
+            event = await ctx.client.events.wait_for(GuildMessageCreateEvent, predicate=is_author, timeout=timeout)
+        except asyncio.TimeoutError:
+            await ctx.edit_initial_response(timeout_msg)
+            return None
+
+        if event.content == "❌":
+            return None
+
+        if not validator:
+            return event
+
+        elif isinstance(validator, list):
+            if any(valid_resp.lower() == event.content.lower() for valid_resp in validator):
+                return event
+            else:
+                validation_message = await ctx.respond(f"That wasn't a valid response... Expected one these: {' - '.join(validator)}")
+                await asyncio.sleep(3)
+                await validation_message.delete()
+
+        elif asyncio.iscoroutinefunction(validator):
+            valid = await validator(ctx, event)
+            if valid:
+                return event
+            else:
+                validation_message = await ctx.respond("That doesn't look like a valid response... Try again?")
+                await asyncio.sleep(3)
+                await validation_message.delete()
+
+        elif isinstance(validator, Callable):
+            if validator(ctx, event):
+                return event
+            else:
+                validation_message = await ctx.respond(f"Something about that doesn't look right... Try again?")
+                await asyncio.sleep(3)
+                await validation_message.delete()
+
+
+def is_int(ctx, event):
+    try:
+        int(event.content)
+        return event
+    except ValueError:
+        pass
 
 
 @ tanjun.as_loader
